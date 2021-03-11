08b5f74437483408621e5d6173f3bc702d4a39c2
tumtpk <pk.prateep@gmail.com>
2021-03-11 23:46:21 +0700

    show image

--- frontend/views/invasionlist/view.php
+++ frontend/views/invasionlist/view.php
@@ -11,12 +11,20 @@ $this->params['breadcrumbs'][] = ['label' => 'Invasionlists', 'url' => ['index']
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
+
+<style>
+    .card-header {
+        padding: 20px 20px 10px 20px !important;
+    }
+</style>
 <div class="invasionlist-view">
 
-    <h3><?= Html::encode($this->title) ?></h3>
     <div class="row clearfix">
         <div class="col-xl-12 col-lg-12 col-md-12">
             <div class="card">
+                <div class="card-header">
+                    <h3><?= Html::encode($this->title) ?></h3>
+                </div>
                 <div class="card-body ribbon">
                     <div class="row">
                         <div class="col-md-6">
@@ -33,8 +41,8 @@ $this->params['breadcrumbs'][] = $this->title;
                                 ],
                             ]) ?>
                         </div>
-                        <div class="col-md-6">
-                            <img src="https://www.w3schools.com/images/w3schools_green.jpg" alt="" class="img-view">
+                        <div class="col-md-3">
+                            <img src="https://www.spu.ac.th/uploads/webfac/f000000/contents/20180312145944OSQuw1m.jpg" alt="">
                         </div>
                     </div>
                 </div>
